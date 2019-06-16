311c6ac9f30527c16126b957fe07bc1147282a0e
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-16 16:09:16 +1000

    added dark theme ability, created navbar and landing page components

---
+++ src/pages/components/Landing.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Landing() {
+  return (
+    <React.Fragment>
+      <h1>Bradley richardson</h1>
+      <div>Landing page</div>
+    </React.Fragment>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,30 @@
 import React from "react"
+import Nav from "./components/Nav"
+import Landing from "./components/Landing"
+import { ThemeProvider } from "./components/theme"
 
-export default () => <div>Hello world!</div>
+class SpaceDashboard extends React.Component {
+  state = {
+    theme: "light",
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === "light" ? "dark" : "light",
+      }))
+    },
+  }
+
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+            <Nav />
+            <Landing />
+          </div>
+        </div>
+      </ThemeProvider>
+    )
+  }
+}
+
+export default SpaceDashboard
