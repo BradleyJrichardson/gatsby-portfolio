4565b6985a14817f1fa52f425f75902ab2070555
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-17 17:15:45 +1000

    added sticky feature, npm i icons

--- src/pages/components/Landing.js
+++ src/pages/components/Landing.js
@@ -1,4 +1,7 @@
 import React from "react"
+import Sticky from "./Sticky.jsx"
+import "../../styles/sticky.css"
+import ExampleStuckContent from "./ExampleStuckContent.jsx"
 
 export default function Landing() {
   return (
@@ -11,6 +14,15 @@ export default function Landing() {
             <br />
             Current Student at CoderAcademy <h1> 👨‍💻</h1>
             <br />
+            <Sticky>
+              <ExampleStuckContent>
+                <p>
+                  This is a stickable component. It will get a prop called
+                  "modifiers" containing an array of strings like "stuck-top"
+                  when it's been scrolled out of view.
+                </p>
+              </ExampleStuckContent>
+            </Sticky>
           </div>
         </div>
         <div className="down-hand">👇</div>
