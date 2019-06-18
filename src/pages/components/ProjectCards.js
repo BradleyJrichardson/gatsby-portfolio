d033f13ff1b6a0c198d15f9f1c366336825c8d0a
BradleyJrichardson <brdl.richardson@gmail.com>
2019-06-18 17:45:58 +1000

    added particle background

--- src/pages/components/ProjectCards.js
+++ src/pages/components/ProjectCards.js
@@ -1,24 +1,20 @@
 import React from "react"
 import { ThemeConsumer } from "./theme"
 
-export default function ProjectCards() {
+export default function ProjectCards({ title, about, img }) {
   return (
     <ThemeConsumer>
       {({ theme }) => (
         <div className={`card ${theme}`}>
-          <div className="card-heading">card content</div>
+          <div className="card-heading">{title}</div>
           <div className="card-container">
             <div className="card-information">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Perferendis sapiente, delectus voluptas minus tempora quod
-              corporis voluptatum dolor, accusamus officiis fugit modi nemo
-              dolorem excepturi non doloribus? Nulla enim quae recusandae atque
-              illum ipsam consequatur, eos nobis molestias mollitia aperiam ut
-              qui blanditiis consequuntur tenetur porro soluta dolorum nam
-              totam.
+              {about}
               <div className="card-sub-heading">Technologies</div>
             </div>
-            <div className="image-container"> information </div>
+            <div className="image-container">
+              <img src={img} alt="" />
+            </div>
           </div>
         </div>
       )}
